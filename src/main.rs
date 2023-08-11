63097939716fe4e5d3b85e62d8f6b125ff33ef34
DarthChungo <antonio@adaro.eu>
2023-08-12 00:25:08 +0200

    Use exe crate for writing PE sections

--- src/main.rs
+++ src/main.rs
@@ -1,106 +1,94 @@
-use rand::distributions::{Alphanumeric, DistString};
+use exe::Buffer;
+use exe::PE;
 use std::fs;
 use std::io;
 use std::io::Read;
 use std::io::Write;
-use std::os::fd::AsRawFd;
-use std::os::unix::prelude::FileExt;
 
-#[derive(Debug)]
-struct UnifiedKernelImageSection {
-    lenght: u64,
-    name: String,
-    file: fs::File,
+struct UnifiedKernelImage {
+    executable: exe::VecPE,
+    output: String,
 }
 
-impl UnifiedKernelImageSection {
-    fn from_file(path: &str, name: &str) -> io::Result<Self> {
-        let file = fs::File::open(path)?;
-        let lenght = file.metadata()?.len();
+impl UnifiedKernelImage {
+    fn new(stub: &str, output: &str) -> io::Result<Self> {
+        let executable = exe::VecPE::from_disk_file(stub)
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
 
         Ok(Self {
-            lenght,
-            name: name.to_owned(),
-            file,
+            executable,
+            output: output.to_owned(),
         })
     }
 
-    fn from_files(paths: Vec<&str>, name: &str) -> io::Result<Self> {
-        let mut files: Vec<fs::File> = paths
+    fn add_section_buf<T: AsRef<[u8]>>(&mut self, name: &str, data: T) -> Result<(), exe::Error> {
+        let mut sec = exe::ImageSectionHeader::default();
+
+        sec.set_name(Some(name));
+        sec.size_of_raw_data = data.as_ref().len() as u32;
+
+        let sec = self.executable.append_section(&sec)?.to_owned();
+        self.executable
+            .resize(self.executable.len() + data.as_ref().len(), 0);
+
+        sec.write(&mut self.executable, data)?;
+
+        Ok(())
+    }
+
+    fn add_section_path(&mut self, name: &str, path: &str) -> io::Result<()> {
+        let mut file = fs::File::open(path)?;
+        let mut buf: Vec<u8> = Vec::new();
+
+        file.read_to_end(&mut buf)?;
+
+        Ok(self
+            .add_section_buf(name, buf)
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?)
+    }
+
+    fn add_section_paths(&mut self, name: &str, paths: Vec<&str>) -> io::Result<()> {
+        let mut buf: Vec<u8> = Vec::new();
+
+        let files: Vec<fs::File> = paths
             .iter()
             .map(|path| Ok(fs::File::open(path)?))
             .collect::<io::Result<_>>()?;
 
-        let opts = memfd::MemfdOptions::new();
-        let mut file = opts
-            .create(Alphanumeric.sample_string(&mut rand::thread_rng(), 8))
-            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?
-            .into_file();
+        files
+            .iter()
+            .map(|mut f| Ok(f.read_to_end(&mut buf)?))
+            .collect::<io::Result<Vec<usize>>>()?;
 
-        let mut lenght: u64 = 0;
-
-        for f in &mut files {
-            let mut buf = Vec::new();
-            f.read_to_end(&mut buf)?;
-
-            let mut pos = 0;
-
-            while pos < buf.len() {
-                let written = file.write_at(&buf[pos..], lenght + pos as u64)?;
-                pos += written;
-            }
-
-            lenght += pos as u64;
-        }
-
-        file.flush()?;
-        Ok(Self {
-            lenght,
-            name: name.to_owned(),
-            file,
-        })
+        Ok(self
+            .add_section_buf(name, buf)
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?)
     }
 
-    fn get_path(&self) -> String {
-        format!("/proc/self/fd/{}", self.file.as_raw_fd())
-    }
-}
+    fn output(&self) -> io::Result<()> {
+        let buf = self
+            .executable
+            .recreate_image(exe::PEType::Disk)
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
 
-struct UnifiedKernelImage {
-    osrel: fs::File,
-    cmdline: fs::File,
-    splash: fs::File,
-    linux: fs::File,
-    initrd: fs::File,
-    output: fs::File,
-}
+        let mut output = fs::File::options()
+            .write(true)
+            .create(true)
+            .open(&self.output)?;
 
-impl UnifiedKernelImage {
-    fn new(
-        osrel: &str,
-        cmdline: &str,
-        splash: &str,
-        linux: &str,
-        initrds: Vec<&str>,
-        output: &str,
-    ) -> Result<Self, io::Error> {
-        Err(io::Error::new(io::ErrorKind::Other, "todo"))
+        output.write(&buf)?;
+
+        Ok(())
     }
 }
 
 fn main() -> io::Result<()> {
-    let sec0 = UnifiedKernelImageSection::from_file("/etc/kernel/cmdline", "cmdline")?;
+    let mut uki =
+        UnifiedKernelImage::new("/usr/lib/systemd/boot/efi/linuxx64.efi.stub", "test.efi")?;
 
-    let mut sec1 = UnifiedKernelImageSection::from_files(
-        vec!["/boot/initramfs-linux-zen.img", "/boot/intel-ucode.img"],
-        "initrd",
-    )?;
+    uki.add_section_path(".osrel", "/usr/lib/os-release")?;
 
-    dbg!(&sec0);
-    dbg!(&sec1);
-
-    let mut output = fs::File::create("output")?;
-    io::copy(&mut sec1.file, &mut output)?;
+    uki.output()?;
 
     Ok(())
 }
