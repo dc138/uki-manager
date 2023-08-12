c66bbc8b39c2091138749dfc92478190f8e70620
DarthChungo <antonio@adaro.eu>
2023-08-12 16:35:09 +0200

    Fix section offsets & pe metadata

--- src/main.rs
+++ src/main.rs
@@ -24,14 +24,34 @@ impl UnifiedKernelImage {
     fn add_section_buf<T: AsRef<[u8]>>(&mut self, name: &str, data: T) -> Result<(), exe::Error> {
         let mut sec = exe::ImageSectionHeader::default();
 
+        let size: u32 = data
+            .as_ref()
+            .len()
+            .try_into()
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+
+        let virtual_size = size;
+
+        let raw_size = self
+            .executable
+            .align_to_file(exe::Offset(size))
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?
+            .0;
+
         sec.set_name(Some(name));
-        sec.size_of_raw_data = data.as_ref().len() as u32;
+        sec.size_of_raw_data = raw_size;
+        sec.virtual_size = virtual_size;
         sec.characteristics =
             SectionCharacteristics::MEM_READ | SectionCharacteristics::CNT_INITIALIZED_DATA;
 
         let sec = self.executable.append_section(&sec)?.to_owned();
-        self.executable
-            .resize(sec.pointer_to_raw_data.0 as usize + data.as_ref().len(), 0);
+
+        self.executable.resize(
+            (sec.pointer_to_raw_data.0 + raw_size)
+                .try_into()
+                .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?,
+            0,
+        );
 
         sec.write(&mut self.executable, data)?;
 
@@ -67,7 +87,19 @@ impl UnifiedKernelImage {
             .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?)
     }
 
-    fn output(&self) -> io::Result<()> {
+    fn output(&mut self) -> io::Result<()> {
+        self.executable
+            .fix_image_size()
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+
+        let checksum = self
+            .executable
+            .calculate_checksum()
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+
+        let headers = self.executable.get_valid_mut_nt_headers_64().unwrap();
+        headers.optional_header.checksum = checksum;
+
         let buf = self
             .executable
             .recreate_image(exe::PEType::Disk)
@@ -89,10 +121,12 @@ fn main() -> io::Result<()> {
         UnifiedKernelImage::new("/usr/lib/systemd/boot/efi/linuxx64.efi.stub", "output.efi")?;
 
     uki.add_section_path(".osrel", "/usr/lib/os-release")?;
+    uki.add_section_buf(".uname", "6.4.8-zen1-1-zen")
+        .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
     uki.add_section_path(".cmdline", "/etc/kernel/cmdline")?;
     uki.add_section_paths(
         ".initrd",
-        vec!["/boot/initramfs-linux-zen.img", "/boot/intel-ucode.img"],
+        vec!["/boot/intel-ucode.img", "/boot/initramfs-linux-zen.img"],
     )?;
     uki.add_section_path(".linux", "/boot/vmlinuz-linux-zen")?;
 
