984682e9be931281dc7847ba6323f69a838bc50f
DarthChungo <antonio@adaro.eu>
2023-08-21 18:10:59 +0200

    Finish proc macro for kernel config struct

--- uki-manager-proc/src/lib.rs
+++ uki-manager-proc/src/lib.rs
@@ -1,48 +1,94 @@
 use proc_macro as pm;
 use proc_macro2::Span as pm2s;
+use quote::quote;
 use syn::Data as sd;
 use syn::Fields as sf;
 use syn::Ident as si;
+use syn::Meta as sm;
 
-#[proc_macro_attribute]
-pub fn serde_optional(_attr: pm::TokenStream, item: pm::TokenStream) -> pm::TokenStream {
-    let ast = syn::parse_macro_input!(item as syn::DeriveInput);
+#[proc_macro_derive(TomlParseWithDefault, attributes(default))]
+pub fn toml_parse_with_default(item: pm::TokenStream) -> pm::TokenStream {
+    let input = syn::parse_macro_input!(item as syn::DeriveInput);
+    let ident = &input.ident;
 
-    let name = ast.ident;
-    let vis = ast.vis;
-
-    let fields = match ast.data {
+    let fields = match input.data {
         sd::Struct(syn::DataStruct {
             fields: sf::Named(syn::FieldsNamed { ref named, .. }),
             ..
         }) => named,
-        _ => panic!("you may only call this for structs"),
+        _ => panic!("this method should only be called for structs"),
     };
 
-    let old_fields = fields.iter().map(|f| {
-        let name = &f.ident;
+    let fields_option = fields.iter().map(|f| {
+        let ident = &f.ident;
         let ty = &f.ty;
         let vis = &f.vis;
 
-        quote::quote! { #vis #name: #ty }
+        quote::quote! { #vis #ident: Option<#ty> }
     });
 
-    let new_fields = fields.iter().map(|f| {
-        let name = &f.ident;
-        let ty = &f.ty;
-        let vis = &f.vis;
+    let fields_init_option = fields.iter().map(|f| {
+        let ident = &f.ident;
 
-        quote::quote! { #vis #name: Option<#ty> }
+        quote::quote! { #ident: config.#ident.unwrap_or(default.#ident) }
     });
 
-    let new_name = si::new(&format!("{}Option", name), pm2s::call_site());
+    let fields_init_default = fields.iter().map(|f| {
+        let ident = &f.ident;
 
-    pm::TokenStream::from(quote::quote! {
-        #vis struct #name {
-            #(#old_fields,)*
+        let default_value = f
+            .attrs
+            .iter()
+            .find_map(|attr| match attr.meta {
+                sm::List(syn::MetaList {
+                    ref path,
+                    ref tokens,
+                    ..
+                }) => {
+                    match path
+                        .segments
+                        .first()
+                        .expect("attribute path should be non-empty")
+                        .ident
+                        .to_string()
+                        == "default"
+                    {
+                        true => Some(tokens),
+                        false => None,
+                    }
+                }
+                _ => None,
+            })
+            .expect("all fields should have one default name-value attribute");
+
+        quote! { #ident: #default_value }
+    });
+
+    let ident_optional = si::new(&format!("{}Optional", ident), pm2s::call_site());
+
+    pm::TokenStream::from(quote! {
+        #[derive(serde::Deserialize)]
+        struct #ident_optional {
+            #(#fields_option,)*
         }
-        #vis struct #new_name {
-            #(#new_fields,)*
+
+        impl #ident {
+            pub fn parse_with_default(path: String) -> anyhow::Result<Self> {
+                let default = Self {
+                    #(#fields_init_default,)*
+                };
+
+                Self::parse_with_custom_default(path, default)
+            }
+
+            pub fn parse_with_custom_default(path: String, default: Self) -> anyhow::Result<Self> {
+                let content = std::fs::read_to_string(path)?;
+                let config: #ident_optional = toml::from_str(&content)?;
+
+                Ok(Self {
+                    #(#fields_init_option,)*
+                })
+            }
         }
     })
 }
--- uki-manager/src/config.rs
+++ uki-manager/src/config.rs
@@ -1,70 +1,27 @@
-use anyhow as ah;
-use serde::Deserialize;
-use std::fs;
+use uki_manager_proc as ump;
 
+#[derive(ump::TomlParseWithDefault)]
 pub struct KernelConfig {
+    #[default("/efi/EFI/Linux/".to_owned())]
     pub output_dir: String,
+
+    #[default("%name%.efi".to_owned())]
     pub output_name: String,
+
+    #[default("/usr/lib/systemd/boot/efi/linuxx64.efi.stub".to_owned())]
     pub stub_path: String,
+
+    #[default("/etc/kernel/cmdline".to_owned())]
     pub cmdline_path: String,
+
+    #[default(vec!["/boot/amd-ucode.img".to_owned(),
+                   "/boot/intel-ucode.img".to_owned(),
+                   "/boot/initramfs-%name%.img".to_owned()])]
     pub initrd_paths: Vec<String>,
+
+    #[default("/boot/vmlinuz-%name%".to_owned())]
     pub vmlinuz_path: String,
+
+    #[default("/usr/share/systemd/bootctl/splash-arch.bmp".to_owned())]
     pub splash_path: String,
-}
-
-#[derive(Deserialize)]
-pub struct KernelConfigOption {
-    pub output_dir: Option<String>,
-    pub output_name: Option<String>,
-    pub stub_path: Option<String>,
-    pub cmdline_path: Option<String>,
-    pub initrd_paths: Option<Vec<String>>,
-    pub vmlinuz_path: Option<String>,
-    pub splash_path: Option<String>,
-}
-
-impl KernelConfig {
-    pub fn parse_with_default(path: String, default: Self) -> ah::Result<Self> {
-        let content = fs::read_to_string(path)?;
-        let config: KernelConfigOption = toml::from_str(&content)?;
-
-        Ok(Self {
-            output_dir: config.output_dir.unwrap_or(default.output_dir),
-            output_name: config.output_name.unwrap_or(default.output_name),
-            stub_path: config.stub_path.unwrap_or(default.stub_path),
-            cmdline_path: config.cmdline_path.unwrap_or(default.cmdline_path),
-            initrd_paths: config.initrd_paths.unwrap_or(default.initrd_paths),
-            vmlinuz_path: config.vmlinuz_path.unwrap_or(default.vmlinuz_path),
-            splash_path: config.splash_path.unwrap_or(default.splash_path),
-        })
-    }
-
-    pub fn parse_with_global_default(path: String) -> ah::Result<Self> {
-        let content = fs::read_to_string(path)?;
-        let config: KernelConfigOption = toml::from_str(&content)?;
-
-        let default = KernelConfig {
-            output_dir: "/efi/EFI/Linux/".to_owned(),
-            output_name: "%name%.efi".to_owned(),
-            stub_path: "/usr/lib/systemd/boot/efi/linuxx64.efi.stub".to_owned(),
-            cmdline_path: "/etc/kernel/cmdline".to_owned(),
-            initrd_paths: vec![
-                "/boot/amd-ucode.img".to_owned(),
-                "/boot/intel-ucode.img".to_owned(),
-                "/boot/initramfs-%name%.img".to_owned(),
-            ],
-            vmlinuz_path: "/boot/vmlinuz-%name%".to_owned(),
-            splash_path: "/usr/share/systemd/bootctl/splash-arch.bmp".to_owned(),
-        };
-
-        Ok(Self {
-            output_dir: config.output_dir.unwrap_or(default.output_dir),
-            output_name: config.output_name.unwrap_or(default.output_name),
-            stub_path: config.stub_path.unwrap_or(default.stub_path),
-            cmdline_path: config.cmdline_path.unwrap_or(default.cmdline_path),
-            initrd_paths: config.initrd_paths.unwrap_or(default.initrd_paths),
-            vmlinuz_path: config.vmlinuz_path.unwrap_or(default.vmlinuz_path),
-            splash_path: config.splash_path.unwrap_or(default.splash_path),
-        })
-    }
 }
