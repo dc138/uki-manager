e22ab538398dd09e74f585bb9137c78b3e6faee0
DarthChungo <antonio@adaro.eu>
2023-08-28 14:51:57 +0200

    Get list of installed kernels

--- uki-manager/src/main.rs
+++ uki-manager/src/main.rs
@@ -1,11 +1,25 @@
 use std::fs;
 use std::process as proc;
 
+use anyhow::Context;
+
 mod cfg;
 mod opts;
+mod traits;
 mod uki;
 
 static VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
+macro_rules! unwrap_or_continue {
+    ($res:expr) => {
+        match $res {
+            Some(x) => x,
+            None => {
+                continue;
+            }
+        }
+    };
+}
 
 fn main() -> Result<(), anyhow::Error> {
     let opts = opts::parse_opts()?;
@@ -18,9 +32,18 @@ fn main() -> Result<(), anyhow::Error> {
         proc::exit(0);
     }
 
-    let config_str = fs::read_to_string(opts.config)?;
-    let config = cfg::Config::from_str_default(config_str.as_str(), cfg::Config::default())?;
-    dbg!(config);
+    let config_str = fs::read_to_string(opts.config).context("cannot open config file")?;
+    let config = cfg::Config::from_str_default(config_str.as_str(), cfg::Config::default())
+        .context("cannot parse input config file")?;
+
+    for entry in fs::read_dir(config.vm_dir).context("cannot read vm_dir")? {
+        let entry = unwrap_or_continue!(entry.ok());
+        let entry_name = unwrap_or_continue!(entry.file_name().into_string().ok());
+
+        let (_, kernel_name) = unwrap_or_continue!(entry_name.split_once("vmlinuz-"));
+
+        dbg!(kernel_name);
+    }
 
     //let mut uki =
     //uki::UnifiedKernelImage::new("/usr/lib/systemd/boot/efi/linuxx64.efi.stub", "output.efi")?;
